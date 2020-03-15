f472b1ad07bf2e403252a6ddfa63c7140a9138b2
Elderkly <897676943@qq.com>
2020-03-15 16:58:08 +0800

    调整文件结构 加入启动服务快捷方式

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # AlibabaGetDataTool
 基于python 用于爬取国际站数据
+
+##  使用说明
+``` bash
+git clone https://github.com/Elderkly/AlibabaGetDataTool.git
+
+python app.py
+
+open html
+```
--- app.py
+++ app.py
@@ -16,13 +16,14 @@ CORS(app, supports_credentials=True)
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+# 创建线程类，继承Thread类
 class MyThread(threading.Thread):
   def __init__(self, func, args):
-    threading.Thread.__init__(self)
+    threading.Thread.__init__(self) # 调用父类的构造函数
     self.args = args
     self.func = func
  
-  def run(self):
+  def run(self): # 线程活动方法
     apply(self.func, self.args)
 
 # 爬取页面html结构
@@ -71,8 +72,7 @@ def getMaxPage():
                 _page = nextBtn.find_all(attrs={"class": ["next-btn next-btn-normal next-btn-medium next-pagination-item","next-btn next-btn-normal next-btn-medium next-pagination-item current"]})[-1].string
                 if _page !=None:
                     maxPage = int(_page)
-                print('总页数')
-                print(_page)
+                # print('总页数%s'%_page)
         except:
             return {'code':50001,'msg':'地址出错'}
         else:
@@ -103,31 +103,37 @@ def getCommodityList():
 def getList(url, page, maxPage, mainJson):
     if url:
         _url = get_getCommodityList_url(url,page)
-        print(_url)
+        # print(_url)
         soup = BeautifulSoup(get_html(_url), 'lxml')   #初始化BeautifulSoup库,并设置解析器
         box = soup.find(attrs={"class": "component-product-list"})                           #   父节点
         listArray = box.find_all(attrs={"class": ["icbu-product-card vertical large product-item","icbu-product-card vertical large product-item last"]})  #   所有子节点
          # 多线程
         t_start = time.time()
-        threadList = [MyThread(get_deitails_keyword, (item, url, page, maxPage, mainJson, index, len(listArray))) for index,item in enumerate(listArray)]
+        threadList = [MyThread(get_deitails_keyword, (item, url, mainJson, index, len(listArray))) for index,item in enumerate(listArray)]
+        # 调用线程类创建新线程，返回线程列表
         for t in threadList:
-            t.setDaemon(True)
-            t.start()
+            t.setDaemon(True) # 设置守护线程，父线程会等待子线程执行完后再退出
+            t.start() # 线程开启
         for i in threadList:
-            i.join()
-        print('总耗时%s'%(time.time()-t_start))
+            i.join() # 等待线程终止，等子线程执行完后再执行父线程
+        # print('第%s页耗时%s'%(page,(time.time()-t_start)))
+        mainJson = mainJson.sort(key = lambda x:x["time"])
         return mainJson
     else:
+        mainJson = mainJson.sort(key = lambda x:x["time"])
         return mainJson
-
-def get_deitails_keyword(item, url, page, maxPage, mainJson, index, length):
+        
+#   爬取详情页关键词
+def get_deitails_keyword(item, url, mainJson, index, length):
     a = item.find(name = 'a')
     span = item.find(attrs={"class": "title-con"})
+    start_time = time.time()
     if span.string != None:
         keyWord = []
         try:
             details = BeautifulSoup(get_html('https://geqian.en.alibaba.com'+a['href']), 'lxml')
             title = details.find(name = 'title').string
+            # print(title)
             name = re.sub(span.string,'',title,flags=re.IGNORECASE)
             name = name.replace(' - Buy ','')
             name = name.replace(' Product on Alibaba.com','')
@@ -136,13 +142,13 @@ def get_deitails_keyword(item, url, page, maxPage, mainJson, index, length):
             keyWord = []
             print('获取关键词出错 %s' %r)
         else:
-            mainJson.append({'text':span.string, 'keyWord': keyWord})
-    if index == length:               #   当前周期遍历完毕
-        print('第' + str(page) + '页爬取结束')
-        if page != maxPage:                     #   如果不是最后一页则递归遍历 直到所有数据爬取完毕
-            return getList(url, page + 1, maxPage, mainJson)
-        else:
-            return mainJson
+            mainJson.append({'text':span.string, 'keyWord': keyWord, 'time': start_time})
+    # if index == length:               #   当前周期遍历完毕
+    #     print('第' + str(page) + '页爬取结束')
+    #     if page != maxPage:                     #   如果不是最后一页则递归遍历 直到所有数据爬取完毕
+    #         return getList(url, page + 1, maxPage, mainJson)
+    #     else:
+    #         return mainJson
 
 # 爬取商品时根据分页拼接url
 def get_getCommodityList_url(url,page):
@@ -155,6 +161,7 @@ def get_getCommodityList_url(url,page):
         urlArray[3] = urlArray[3][:-2] + '-' + str(page)
     _url = '/'.join(urlArray)
     return _url
+
 #   查询关键词
 @app.route('/getKeyWord')
 def getKeyWord():
@@ -164,11 +171,11 @@ def getKeyWord():
         try:
             details = BeautifulSoup(get_html(url), 'lxml')
             title = details.find(name = 'title').string
-            print(title)
+            # print(title)
             name = re.sub(details.find(attrs = {'class': 'ma-title'}).string,'',title,flags=re.IGNORECASE)
             name = name.replace(' - Buy ','')
             name = name.replace(' Product on Alibaba.com','')
-            print(name)
+            # print(name)
             keyWordArray = name.split(',')
         except Exception as r:
             print('getKeyWordError%s' %r)
@@ -180,8 +187,8 @@ def getKeyWord():
 
 if __name__ == "__main__":
     app.run(
-    host = '0.0.0.0',#任何ip都可以访问
-    port = 5000,#端口
-    debug = True
-)
+        host = '0.0.0.0',#任何ip都可以访问
+        port = 5000,#端口
+        debug = False
+    )
 
